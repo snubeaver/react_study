2392bb1fe3e38733289cf3e80c20ae5714554024
Yoo Jin Young <young@yoojinyoung.com>
2018-09-07 19:05:44 +0900

    Loading states

--- src/App.js
+++ src/App.js
@@ -7,24 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: [
-        {
-          title: "Matrix",
-          poster: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg",
-        },
-        {
-          title: "Iron man",
-          poster: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513",
-        },
-        {
-          title: "Oldboy",
-          poster: "https://scdn.nflximg.net/ipl/13235/513a4a62b1050e6c7fcf15ad6a119f0bc2fbd072.jpg",
-        },
-        {
-          title: "Star wars",
-          poster: "https://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg?src=gp&w=300&h=375",
-        },
-      ]
+      movies: null,
     }
   }
 
@@ -32,7 +15,22 @@ class App extends Component {
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "Matrix",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg",
+          },
+          {
+            title: "Iron man",
+            poster: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513",
+          },
+          {
+            title: "Oldboy",
+            poster: "https://scdn.nflximg.net/ipl/13235/513a4a62b1050e6c7fcf15ad6a119f0bc2fbd072.jpg",
+          },
+          {
+            title: "Star wars",
+            poster: "https://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg?src=gp&w=300&h=375",
+          },
           {
             title: 'One Piece',
             poster: 'https://i.ebayimg.com/images/g/vCAAAOSw1ZBUyby5/s-l300.jpg'
@@ -42,12 +40,17 @@ class App extends Component {
     }, 2000);
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
