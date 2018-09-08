5b925bc9e6d55648ddc67a59fe0df68b01d2c18b
Yoo Jin Young <young@yoojinyoung.com>
2018-09-08 12:29:07 +0900

    Async, Await in React

--- src/App.js
+++ src/App.js
@@ -12,17 +12,28 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
     })
     return movies
+  }
+
+   _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies: movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
