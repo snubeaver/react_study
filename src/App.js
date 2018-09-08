39622e2f8611e48d0b1f6ee0fc3730dc086c5530
Ealoglod <zwinzwin@snu.ac.kr>
2018-09-08 17:30:49 +0900

    initial comm

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie';
+
+class App extends Component {
+
+  state = {}
+
+  componentDidMount(){
+    this._getMovies();
+  }
+
+  _renderMovie = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres = {movie.genres}
+        synopsis = {movie.synopsis}
+      />
+    })
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    console.log('did render')
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovie() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie_Columns">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Moive_Columns">
+                <h1>{title}</h1>
+                <div className="Moive_Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie_Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+function MoviePoster({poster, alt}){
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie_Genre">{genre}</span>
+    )
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
+
+export default Movie;
