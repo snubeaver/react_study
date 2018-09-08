69490bd3f6d9b67393da612874443fa48976fb94
Yoo Jin Young <young@yoojinyoung.com>
2018-09-08 12:08:15 +0900

    Promise

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log(fetch('https://yts.am/api/v2/list_movies.json'))
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
