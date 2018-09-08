e7154dbe802076109c7f298122b31f821989f541
Yoo Jin Young <young@yoojinyoung.com>
2018-09-08 11:47:43 +0900

    Ajax in React

--- src/App.js
+++ src/App.js
@@ -12,32 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/51FhdGyJ6DL.jpg",
-          },
-          {
-            title: "Iron man",
-            poster: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/2/25/Iron_Man_poster_3.png/revision/latest?cb=20120420114513",
-          },
-          {
-            title: "Oldboy",
-            poster: "https://scdn.nflximg.net/ipl/13235/513a4a62b1050e6c7fcf15ad6a119f0bc2fbd072.jpg",
-          },
-          {
-            title: "Star wars",
-            poster: "https://imgc.allpostersimages.com/img/posters/star-wars-the-force-awakens-one-sheet_u-L-F8G58H0.jpg?src=gp&w=300&h=375",
-          },
-          {
-            title: 'One Piece',
-            poster: 'https://i.ebayimg.com/images/g/vCAAAOSw1ZBUyby5/s-l300.jpg'
-          }
-        ]
-      })
-    }, 2000);
+    console.log(fetch('https://yts.am/api/v2/list_movies.json'))
   }
 
   _renderMovies = () => {
